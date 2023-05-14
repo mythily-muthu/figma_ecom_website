bfd8e3ef15cedeff66ae55880e64c9014ed2860c
mythily-muthu <mythilimuthu18@gmail.com>
2023-05-14 12:18:55 +0530

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="">
+
+      <Router>
+        {/* layout */}
+        {/* navbar */}
+        <Navbar />
+        {/* content */}
+        <Content />
+      </Router>
     </div>
   );
 }
